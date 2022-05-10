2dbcb244237ff1fde8b4052686a582fe7f5f0a2b
aneylon <arlen.m.neylon@gmail.com>
2022-05-10 14:59:34 -0600

    Add component code.

--- udemy/components/src/index.js
+++ udemy/components/src/index.js
@@ -7,10 +7,18 @@ import ApprovalCard from './ApprovalCard'
 const App = () => {
     return(
     <div className="ui container comments">
-    <ApprovalCard/>
-    <CommentDetail author="Dude" postTime="now" commentText="something" image={faker.image.avatar()}/>
-    <CommentDetail author="Man" postTime="then" commentText="nothing" image={faker.image.avatar()}/>
-    <CommentDetail author="Bro" postTime="later" commentText="whatever" image={faker.image.avatar()}/>
+        <ApprovalCard>
+            Testers
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail author="Dude" postTime="now" commentText="something" image={faker.image.avatar()}/>
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail author="Man" postTime="then" commentText="nothing" image={faker.image.avatar()}/>
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail author="Bro" postTime="later" commentText="whatever" image={faker.image.avatar()}/>
+        </ApprovalCard>
     </div>
     )
 }
