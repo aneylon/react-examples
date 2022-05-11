b8756236c77b432faea8c06c89796c7434ae94b9
Arlen Neylon <arlen.m.neylon@gmail.com>
2022-05-11 16:46:38 -0600

    Conditional rendering.

--- udemy/seasons/src/index.js
+++ udemy/seasons/src/index.js
@@ -6,27 +6,45 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     // only time to directly assign to state
-    this.state = { lat: null, lon: null }
+    this.state = { lat: null, lon: null, errorMessage: '' }
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({lat: position.coords.latitude, lon: position.coords.longitude}) 
         console.info(position)
       },
-      (err) => console.error(err)
+      (err) => {
+        console.error(err)
+        this.setState({errorMessage: err.message})
+      }
     )
   }
 
   render () {
+    console.log('render')
     return (
       <div>
         <div>
-          <p>
-            Latitude: {this.state.lat}
-          </p>
-          <p>
-            Longitude: {this.state.lon}
-          </p>
+          { (this.state.lat !== null && this.state.lon !== null) &&
+            <div>
+              <p>
+                Latitude: {this.state.lat}
+              </p>
+              <p>
+                Longitude: {this.state.lon}
+              </p>
+            </div>
+          }
+          { this.state.errorMessage !== '' && 
+            <p>
+              Error: {this.state.errorMessage}
+            </p>
+          }
+          { (this.state.lon === null && this.state.lat === null && this.state.errorMessage === '') &&
+            <p>
+              ...Loading...
+            </p>
+          }
         </div>
         <SeasonDisplay/>
       </div>
