aac56a8117d7c7f52b1a13b2522400030a7f334d
aneylon <arlen.m.neylon@gmail.com>
2022-05-12 15:14:56 -0600

    Icons and text.

--- udemy/seasons/src/SeasonDisplay.js
+++ udemy/seasons/src/SeasonDisplay.js
@@ -1,15 +1,37 @@
 import React from 'react'
 
-const getSeason = (lat, month) => {
+const seasonConfig = {
+  summer: {
+    text: 'Lets hit the beach',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr it is cold',
+    iconName: 'snowflake'
+  }
+}
 
+const getSeason = (lat, month) => {
+  if(month > 2 && month < 9){
+    return lat > 0 ? 'summer' : 'winter'
+  }
+  return lat > 0 ? 'winter' : 'summer'
 }
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth())
-  
+  console.log(season)
+  // const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'
+  // const icon = season === 'winter' ? 'snowflake' : 'sun'
+  const { text, iconName } = seasonConfig[season]
+
   return (
     <div>
-      Season Display {props.lat} {props.lon}
+      <i className={`${iconName} icon`} />
+      <h1>
+        {text}    
+      </h1>
+      <i className={`${iconName} icon`} />
     </div>)
 }
 
