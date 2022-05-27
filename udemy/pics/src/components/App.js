702b42f3cd698119f71d01c48395044d56344568
Arlen Neylon <arlen.m.neylon@gmail.com>
2022-05-27 16:25:22 -0600

    get images and display in list.

--- udemy/pics/src/components/App.js
+++ udemy/pics/src/components/App.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import axios from 'axios'
+
+import unsplash from '../api/unsplash'
 
 import SearchBar from './SearchBar'
+import ImageList from './ImageList'
 
 // const App = () => {
 class App extends React.Component {
+  state = { images : [] }
 
-  onSearchSubmit(term) {
-    console.log(term)
+  onSearchSubmit = async (term) => {
+    // console.log(term)
+    // let rootUrl = 'https://api.unsplash.com'
+    let endPoint = '/search/photos'
+    const response = await unsplash.get(endPoint, {
+      params: { query: term }
+    })
+    // axios.get(`${rootUrl}${endPoint}`, {
+    // const response = await axios.get(`${rootUrl}${endPoint}`, {
+    //   params: {
+    //     query: term
+    //   },
+    // })
+      // .then((response) => {
+      //   console.log(response.data.results)
+      // })
+
+    this.setState({ images: response.data.results })
   }
 
   render() {
@@ -16,6 +37,7 @@ class App extends React.Component {
         style={{ marginTop: '10px' }}
         >
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        <ImageList images={this.state.images}/>
       </div>
     )
   }
