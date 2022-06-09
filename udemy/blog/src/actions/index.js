f0962da0d21c5ef7970efe9d7c4443cd2df41981
Arlen Neylon <arlen.m.neylon@gmail.com>
2022-06-09 11:09:49 -0600

    Set up blog project.

---
+++ udemy/blog/src/actions/index.js
@@ -0,0 +1,18 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder"
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts')
+
+  dispatch({ type: 'FETCH_POSTS', payload: response })
+}
+
+// export const fetchPosts = () => {
+//   return async (dispatch) => {
+//     const response = jsonPlaceholder.get('/posts')
+
+//     dispatch({
+//       type: 'FETCH_POSTS',
+//       payload: response
+//     })
+//   }
+// }
