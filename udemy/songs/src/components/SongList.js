d0e9581aeb1e41321baa919fb1fe338e36f3c6a5
Arlen Neylon <arlen.m.neylon@gmail.com>
2022-06-09 08:59:27 -0600

    Complete songs.

--- udemy/songs/src/components/SongList.js
+++ udemy/songs/src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { selectSong } from '../actions'
 
 class SongList extends Component {
   renderList() {
@@ -7,7 +8,9 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              onClick={() => this.props.selectSong(song)}
+              className="ui button primary">Select</button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -16,7 +19,6 @@ class SongList extends Component {
   }
 
   render() {
-    // console.log(this.props)
     return (
       <div className="ui divided list">
         {this.renderList()}
@@ -26,8 +28,9 @@ class SongList extends Component {
 }
 
 const mapStateToProps = (state) => {
-  // console.log(state)
   return { songs: state.songs }
 }
 
-export default connect(mapStateToProps)(SongList)
+export default connect(mapStateToProps, {
+  selectSong: selectSong
+})(SongList)
