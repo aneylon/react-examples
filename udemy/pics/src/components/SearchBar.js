a4e3340529ac4ebb13d8e69f086e780434275023
aneylon <arlen.m.neylon@gmail.com>
2022-06-14 12:36:56 -0600

    Start code camp mern project.

--- udemy/pics/src/components/SearchBar.js
+++ udemy/pics/src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = { searchTerm : 'testers' }
+  state = { searchTerm : '' }
   // onInputChange (event) {
   //   console.log('change it!', event.target.value)
   // }
